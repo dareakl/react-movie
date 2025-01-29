a7193d740e128cf686c5e415c4d2057502801ade
Sathish Raja <dareakl@users.noreply.github.com>
2025-01-29 14:23:05 +0800

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+
+function App() {
+  return (
+    <>
+      <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+    </>
+  );
+}
+
+export default App;
